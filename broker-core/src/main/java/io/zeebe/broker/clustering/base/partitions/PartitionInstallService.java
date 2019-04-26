6410fc36e234f7ee95570e8fff4d0e8e79523c1e
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2019-04-26 14:32:33 +0200

    start all services

--- broker-core/src/main/java/io/zeebe/broker/clustering/base/partitions/PartitionInstallService.java
+++ broker-core/src/main/java/io/zeebe/broker/clustering/base/partitions/PartitionInstallService.java
@@ -37,6 +37,7 @@ import io.zeebe.broker.logstreams.state.StateStorageFactoryService;
 import io.zeebe.broker.system.configuration.BrokerCfg;
 import io.zeebe.distributedlog.StorageConfiguration;
 import io.zeebe.distributedlog.impl.DistributedLogstreamPartition;
+import io.zeebe.distributedlog.impl.replication.LogReplicationService;
 import io.zeebe.logstreams.impl.service.LeaderOpenLogStreamAppenderService;
 import io.zeebe.logstreams.impl.service.LogStreamServiceNames;
 import io.zeebe.logstreams.log.LogStream;
@@ -127,6 +128,16 @@ public class PartitionInstallService extends Actor
             .dependency(ATOMIX_JOIN_SERVICE)
             .group(LEADERSHIP_SERVICE_GROUP)
             .install();
+
+    final LogReplicationService logReplicationService = new LogReplicationService();
+
+    partitionInstall
+        .createService(
+            ServiceName.newServiceName("log.replication.sender." + logName, Void.class),
+            logReplicationService)
+        .dependency(ATOMIX_SERVICE, logReplicationService.getAtomixInjector())
+        .dependency(logStreamServiceName, logReplicationService.getLogStreamInjector())
+        .install();
 
     partitionInstall.install();
 
